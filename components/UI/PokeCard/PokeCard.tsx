4a3fe171d9f4af39916b2c902ab6720e0ca0abef
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2023-02-25 17:58:40 +0100

    f

--- components/UI/PokeCard/PokeCard.tsx
+++ components/UI/PokeCard/PokeCard.tsx
@@ -6,7 +6,7 @@ export const PokeCard = ({name, image, id}: PokeCardProps) => {
         <div className="m-5">
             <Link href={{ pathname: 'detail', query: { id }}}>
                 <div className="flex flex-col justify-center items-center w-[315px] h-[271px] border-[6px] border-[#FEF2A4] bg-white border-gray-300 border p-3">
-                    <img width='176px' height='176px' src={image} alt={name}/>
+                    <img className="object-cover w-[176px] h-[176px]" width='176px' height='176px' src={image} alt={name}/>
                     <h5 className="text-xl text-center capitalize">{name}</h5>
                 </div>
             </Link>
