e7f8563e1a425b5959dcf20931c2848f492300d0
Martin Průša <prusamartin.pm@gmail.com>
2023-03-10 08:50:18 +0100

    next dynamic route

--- components/UI/PokeCard/PokeCard.tsx
+++ components/UI/PokeCard/PokeCard.tsx
@@ -4,7 +4,8 @@ import Link from "next/link";
 export const PokeCard = ({name, image, id}: PokeCardProps) => {
     return (
         <div className="m-5">
-            <Link href={{ pathname: 'detail', query: { id }}}>
+            {/*<Link href={{ pathname: 'detail', query: { id }}}>*/}
+            <Link href={{ pathname: `./${id}` }}>
                 <div className="flex flex-col justify-center items-center w-[315px] h-[271px] border-[6px] border-[#FEF2A4] bg-white border-gray-300 border p-3">
                     <img className="object-cover w-[176px] h-[176px]" width='176px' height='176px' src={image} alt={name}/>
                     <h5 className="text-xl text-center capitalize">{name}</h5>
